3a0d65939a6f51702ce64ad978d3c09cea6b4c31
Aaron Brown <aaron.brown@droneup.com>
2024-12-13 08:53:10 -0700

    scenario 1

--- spec/euler/level1/problem001_spec.clj
+++ spec/euler/level1/problem001_spec.clj
@@ -7,9 +7,18 @@
 
 (describe "Euler Problem #1"
 
+  ;; (it "finds multiples of 3 and 5")
+
   (it "Solves #1"
+    (should= 0 (euler-1 0))
+    (should= 0 (euler-1 1))
+    (should= 0 (euler-1 2))
+    (should= 0 (euler-1 3))
+    (should= 3 (euler-1 4))
+    (should= 3 (euler-1 5))
+    (should= 8 (euler-1 6))
     (should= 23 (euler-1 10))
-	(should= -1 (euler-1 1000)))
+	  (should= 233168 (euler-1 1000)))
   )
 
 (run-specs)
--- src/euler/level1/problem001.clj
+++ src/euler/level1/problem001.clj
@@ -1,4 +1,10 @@
 (ns euler.level1.problem001)
 
+(defn multiple-of-3-or-5 [n]
+  (or (= 0 (rem n 3))
+      (= 0 (rem n 5))))
+
 (defn euler-1 [n]
-  )
+  (->> (range n)
+       (filter multiple-of-3-or-5)
+       (apply +)))
