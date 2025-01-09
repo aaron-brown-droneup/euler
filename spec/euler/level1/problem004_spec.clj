2281e01765548be833d2f08b2da917ffeb03af60
Aaron Brown <aaron.brown@droneup.com>
2025-01-09 12:18:56 -0700

    euler 4

--- spec/euler/level1/problem004_spec.clj
+++ spec/euler/level1/problem004_spec.clj
@@ -7,8 +7,20 @@
 
 (describe "Euler Problem #4"
 
-  (it "Solves #4"
-    (should= -1 (euler-4 -1)))
-  )
+          (it "Solves #4"
+              (should= 906609 (euler-4))
+
+              )
+
+          (it "can identify palindrome"
+              (should= true (is-palindrome 9009))
+              )
+          (it "can identify non palindrome"
+              (should= false (is-palindrome 100))
+              )
+          (it "can multiply two arbitrary sets"
+              (should= '(1 2 3 4 5 6 7 8 9 2 4 6 8 10 12 14 16 18 3 6 9 12 15 18 21 24 27 4 8 12 16 20 24 28 32 36 5 10 15 20 25 30 35 40 45 6 12 18 24 30 36 42 48 54 7 14 21 28 35 42 49 56 63 8 16 24 32 40 48 56 64 72 9 18 27 36 45 54 63 72 81) (mult-all-with-lim 1 10))
+              )
+          )
 
 (run-specs)
--- src/euler/level1/problem004.clj
+++ src/euler/level1/problem004.clj
@@ -1,4 +1,20 @@
 (ns euler.level1.problem004)
 
-(defn euler-4 [n]
+
+
+
+(defn is-palindrome [n]
+  (= (str n) (apply str (reverse (str n))))
+  )
+
+(defn mult-all-with-lim [min max]
+  (for [x1 (range min max)
+        x2 (range min max)]
+    (* x1 x2))
+  )
+
+(defn euler-4 []
+
+  (apply max (filter (fn [x] (is-palindrome x)) (mult-all-with-lim 100 1000)))
+
   )
